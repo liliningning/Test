b4f6349794e54c40838fc16b23dffc4a748f2b04
liliningning <2914352881@qq.com>
2023-12-21 15:01:56 +0800

    [修改]完善链表的初始化接口

--- linkedList/linkList.c
+++ linkedList/linkList.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 // 状态码
 enum STATUS_CODE
 {
@@ -18,22 +17,46 @@ enum STATUS_CODE
 // 链表的初始化
 int linkListInit(linkList **pList)
 {
-    //给链表开辟一个空间
- linkList * List =(linkList *)malloc(sizeof(linkList));
-
+    // 给链表开辟一个空间
+    linkList *list = (linkList *)malloc(sizeof(linkList));
+    if(list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(list, 0, sizeof(linkList));
+    
+    //给头结点开辟空间
+    linkNode * head =(linkNode *) malloc(sizeof(linkNode));
+    if(list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(list->head, 0, sizeof(linkNode));
+    //初始化
+    list->head->data = 0;
+    list->head->next = NULL;
+    list->len = 0;
+    pList = *list;
+    return ON_SUCCESS;
 }
 
 // 链表的头插
 int linkListHeadInit(linkList *pList, int val)
 {
-
+    linkListPositionInit(pList, 0, val);
 }
 
 // 链表的尾插
-int linkListTailInit(linkList *pList, int val);
+int linkListTailInit(linkList *pList, int val)
+{
+    linkListPositionInit(pList, pList->len, val);
+}
 
 // 链表的指定位置插入
-int linkListPositionInit(linkList *pList, int pos, ELEMENTTYPDE val);
+int linkListPositionInit(linkList *pList, int pos, ELEMENTTYPDE val)
+{
+    
+}
 
 // 链表的指定位置删除
 int linkListPositionDel(linkList *pList, int pos);
@@ -55,4 +78,3 @@ int linkListGetDate(linkList *pList);
 
 // 链表的销毁
 int linkListDestroy(linkList *pList);
-
